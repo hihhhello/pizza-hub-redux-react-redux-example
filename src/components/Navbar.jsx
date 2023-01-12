77248c73bc79bf15cad514c14033a57a9c075df4
Anton Ratushniak <anton.rathusniak-extern@campusjaeger.de>
2023-01-12 15:04:41 +0700

    clean up code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,38 @@
-import { AppBar, Box, Link, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
 import LocalPizzaIcon from '@mui/icons-material/LocalPizza';
-
-import { NavbarCartIconButton } from '../features/NavbarCartIconButton';
 import { NavLink } from 'react-router-dom';
 
-export const Navbar = () => {
-  return (
-    <AppBar component="nav" sx={{ backgroundColor: 'error.light' }}>
-      <Toolbar sx={{ justifyContent: 'space-between' }}>
-        <NavLink to="/">
-          <Box
+import { NavbarCartIconButton } from '../features/NavbarCartIconButton';
+
+export const Navbar = () => (
+  <AppBar component="nav" sx={{ backgroundColor: 'error.light' }}>
+    <Toolbar sx={{ justifyContent: 'space-between' }}>
+      <NavLink to="/">
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'flex-end',
+            color: 'common.white',
+          }}
+        >
+          <LocalPizzaIcon sx={{ fontSize: 48 }} />
+
+          <Typography
             sx={{
-              display: 'flex',
-              alignItems: 'flex-end',
-              color: 'common.white',
+              fontSize: 48,
+              lineHeight: '48px',
+              fontWeight: 800,
+              textDecoration: 'none',
             }}
           >
-            <LocalPizzaIcon sx={{ fontSize: 48 }} />
+            PizzaHub
+          </Typography>
+        </Box>
+      </NavLink>
 
-            <Typography
-              sx={{
-                fontSize: 48,
-                lineHeight: '48px',
-                fontWeight: 800,
-                textDecoration: 'none',
-              }}
-            >
-              PizzaHub
-            </Typography>
-          </Box>
-        </NavLink>
-
-        <NavLink to="cart">
-          <NavbarCartIconButton />
-        </NavLink>
-      </Toolbar>
-    </AppBar>
-  );
-};
+      <NavLink to="cart">
+        <NavbarCartIconButton />
+      </NavLink>
+    </Toolbar>
+  </AppBar>
+);
